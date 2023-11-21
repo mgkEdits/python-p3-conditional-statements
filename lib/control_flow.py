fc5d5d962de5fbced710a15fb973769577cf92ff
mgkEdits <elijahnyasiando.mn@gmail.com>
2023-11-21 14:16:17 +0300

    cmpltd lab

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,39 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
+    if (username.upper() == "ADMIN" and password == "12345"):
+        return "Access granted"
+    else:
+        return "Access denied"
     pass
 
 def hows_the_weather(temperature):
-    # your code here
+    if temperature < 40:
+        response = "brisk"
+    elif 40 <= temperature <= 65:
+        response = "a little chilly"
+    elif temperature > 85:
+        response = "too dang hot"
+    else:
+        response = "perfect"
+    return f"It's {response} out there!"
     pass
 
 def fizzbuzz(num):
-    # your code here
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
     pass
 
 def calculator(operation, num1, num2):
-    # your code here
+    if operation in ["+", "-", "*", "/"]:
+        return eval(f"{num1} {operation} {num2}")
+    else:
+        print("Invalid operation!")
+        return None
     pass
